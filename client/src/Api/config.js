7a7c9ce2515a6b538b7421f9e5ab9720bc86dda2
gourabofficial <gangulygourab3@gmail.com>
2025-06-12 23:33:16 +0530

    feat: Configure axios instance with Clerk authentication and add Vercel configuration files for client and server

--- client/src/Api/config.js
+++ client/src/Api/config.js
@@ -1,11 +1,19 @@
 import axios from "axios";
+import { useClerkAuth } from "@clerk/clerk-react";
+
+const { getToken } = useClerkAuth();
+const token = await getToken();
+
+const headers = {
+  "Content-Type": "application/json",
+};
+
+headers.Authorization = `Bearer ${token}`;
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_HOST,
   withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  headers: headers,
 });
 
 export default axiosInstance;
