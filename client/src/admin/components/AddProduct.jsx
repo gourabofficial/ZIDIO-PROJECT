d6dbb5440f3a00d43bffdcf3f65e293d51d74789
gourabofficial <gangulygourab3@gmail.com>
2025-06-14 00:04:13 +0530

    #Production 11

--- client/src/admin/components/AddProduct.jsx
+++ client/src/admin/components/AddProduct.jsx
@@ -265,7 +265,7 @@ const AddProduct = () => {
       });
 
       // Use the FormData object with correctly appended files and fresh token
-      const response = await AdminAddProduct(productFormData, freshToken);
+      const response = await AdminAddProduct(productFormData, token);
 
       if (!response.success) {
         toast.error(
